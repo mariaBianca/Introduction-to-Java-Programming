954ebdf10ee51724a5ff779d3ef235cdbfe709ee
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-01-14 17:58:27 +0100

    Update Cart.java

--- Assignment2/Cart.java
+++ Assignment2/Cart.java
@@ -1,7 +1,6 @@
 package Assignment2;
 /**
- * @author CONRAD-UWAILA EKHATOR 790213-5396(has talked to Musard regarding another time for presentation due to personal issues)
- * @author MARIA-BIANCA CINDROI 920213-0028
+ * @author MARIA-BIANCA CINDROI
  */
 
 
